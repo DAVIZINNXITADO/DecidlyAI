89238bb62ed3a33a4730ce6db5b4f012b96407bd
DAVIZINNXITADO <davigoncalvesnoronhapbu02@gmail.com>
2026-09-05 16:33:50 -0300

    Atualizar o login.tsx

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -490,8 +490,8 @@ function LoginPage() {
           <img
             src="/DIcon.ico"
             alt="DecidlyIA"
-            className="h-11 w-11 shrink-0 object-contain"
-          />
+            className="h-11 w-11 shrink-0 translate-y-[-2px] object-contain"
+  />
 
           {/* TEXTO ENCOSTADO E CENTRALIZADO */}
 
